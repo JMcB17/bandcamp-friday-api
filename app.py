c70b6c3fb8a6e5b51a39509d0f66855c02d50ff8
JMcB <joel.mcbride1@live.com>
2021-11-13 11:45:53 +0000

    Refactor

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ from flask import Flask, jsonify
 from flask_caching import Cache
 
 
+# add more info as json
+# v1
+# standalone func
+# remove flask_caching
+
+
 DAY = timedelta(days=1)
 
 
@@ -15,20 +21,48 @@ app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 
 
+def get_data_fundraisers() -> dict:
+    response = requests.get('https://isitbandcampfriday.com')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    bandcamp_friday_vm = soup.find('div', id='bandcamp-friday-vm')
+    data_fundraisers = json.loads(bandcamp_friday_vm['data-fundraisers'])[0]
+
+    return data_fundraisers
+
+
+def format_response(data_fundraisers: dict) -> dict:
+    next_bandcamp_friday = datetime.strptime(data_fundraisers['date'], '%a, %d %b %Y %H:%M:%S %z')
+    next_end = next_bandcamp_friday + DAY
+    now = datetime.now(tz=next_bandcamp_friday.tzinfo)
+    it_is = (next_bandcamp_friday < now < next_end)
+
+    response = {
+        'it_is': it_is,
+        'next_start': next_bandcamp_friday.timestamp(),
+        'next_end': next_end.timestamp(),
+        'now': now.timestamp(),
+        'data_fundraisers': data_fundraisers,
+    }
+    return response
+
+
+def is_it_bandcamp_friday() -> bool:
+    data_fundraisers = get_data_fundraisers()
+    info_dict = format_response(data_fundraisers)
+    return info_dict['it_is']
+
+
 class CachedFriday:
     def __init__(self):
         self.next: datetime = datetime.fromtimestamp(0)
 
-    def is_it_bandcamp_friday(self):
-        response = requests.get('https://isitbandcampfriday.com')
-        soup = BeautifulSoup(response.content, 'html.parser')
-        bandcamp_friday_vm = soup.find('div', id='bandcamp-friday-vm')
-        data_fundraisers = json.loads(bandcamp_friday_vm['data-fundraisers'])[0]
+    def cached_is_it_bandcamp_friday(self) -> bool:
+        data_fundraisers = get_data_fundraisers()
+        info_dict = format_response(data_fundraisers)
 
-        next_bandcamp_friday = datetime.strptime(data_fundraisers['date'], '%a, %d %b %Y %H:%M:%S %z')
-        next_end = next_bandcamp_friday + DAY
-        now = datetime.now(tz=next_bandcamp_friday.tzinfo)
-        it_is = (next_bandcamp_friday < now < next_end)
+        it_is = info_dict['it_is']
+        next_end = info_dict['next_end']
+        next_bandcamp_friday = info_dict['next_start']
 
         if it_is:
             self.next = next_end
@@ -55,7 +89,7 @@ cached_friday = CachedFriday()
 @app.route('/isitbandcampfriday')
 @cache.cached(unless=cached_friday.cache_check)
 def api_view():
-    return jsonify(cached_friday.is_it_bandcamp_friday())
+    return jsonify(cached_friday.cached_is_it_bandcamp_friday())
 
 
 if __name__ == "__main__":
